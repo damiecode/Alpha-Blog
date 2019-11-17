e8661ff7d8d0f3bd4de2f7e0d3cfce974c9b8e60
Ale Damilola <damidigi@yahoo.com>
2019-11-17 13:22:24 +0100

    Add secure password and attributes to users

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class PageController < ApplicationController
-  def home
-    
-  end
-  def about
-    
-  end
+  def home; end
+
+  def about; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :articles
-  before_save {self.email = email.downcase}
-  validates :name, presence: true , length: {minimum:3, maximum:25}, uniqueness: { case_sensitive: false }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 105 }, uniqueness:{ case_sensitive: false }, format: { with: VALID_EMAIL_REGEX } 
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has
 end
