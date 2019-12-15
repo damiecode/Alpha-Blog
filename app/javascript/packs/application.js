199b8e573fe9122edfdada0ed57625f731725565
damiecode <damidigi@yahoo.com>
2019-12-15 11:40:16 +0100

    Update gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ var jQuery = require("jquery")
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
 require('bootstrap');
+import "../../assets/stylesheets/application.css"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
